c2b6d823e6b791012c8b428f00be7107100e569f
Haralan Dobrev <hkdobrev@gmail.com>
2014-05-17 13:48:44 +0300

    :memo: Add javadoc to game.invader.Invader

--- Game/src/game/invader/Invader.java
+++ Game/src/game/invader/Invader.java
@@ -8,14 +8,50 @@ import game.space.Space;
 import game.Shared;
 import game.MovablePanel;
 
+/**
+ * I am an invader.
+ * I am a MovablePanel:
+ *   * I move
+ *   * I have a position (x,y)
+ *   * I have a direction (x,y)
+ *   * I have a speed (pixels in direction per move)
+ *
+ * I fly from the top of the space to the bottom.
+ * I can create myself at a random column of the space.
+ * I have the looks of an invader as well (with an Image).
+ */
 public class Invader extends MovablePanel {
 
+    /**
+     * Constant holding the relative path to the invader image
+     */
     private static final String ICON = "invader.jpg";
 
+    /**
+     * Holding the Image instance of the invader.
+     * It should be repainted on every paint (with paintComponent())
+     * so it is always visible even if the invader moves.
+     */
     private Image image;
+
+    /**
+     * Indicate if the invader is visible.
+     */
     private boolean visible;
+
+    /**
+     * Dimensions of the invader - its width and height.
+     */
     private Dimension dim;
 
+    /**
+     * Constructor to initialize the invader.
+     *
+     * Set the Image instance from the ICON.
+     * Set the preferred size with Dimension based on the Shared constants.
+     * Position at the top of the space at random column.
+     * Make visible.
+     */
     public Invader() {
         // Set an image
         ImageIcon icon = new ImageIcon(this.getClass().getResource(ICON));
@@ -31,14 +67,31 @@ public class Invader extends MovablePanel {
         setSpeed(Shared.INVADER_SPEED);
     }
 
+    /**
+     * Get the image instance.
+     *
+     * @return Image
+     */
     public Image getImage() {
         return image;
     }
 
+    /**
+     * Check if invader is visible
+     *
+     * @return boolean
+     */
     public boolean isVisible() {
         return visible;
     }
 
+    /**
+     * Overriden method from MovablePanel.
+     * The invader should move only it is visible - no need to move it after
+     * it left the space.
+     *
+     * This will hide the invader if it left the space after super.performMove()
+     */
     @Override
     protected void performMove() {
         if (!visible) {
@@ -58,6 +111,13 @@ public class Invader extends MovablePanel {
         }
     }
 
+    /**
+     * Perform additional paint operations on every JPanel repaint()
+     * This is automatically called from the super class
+     * when a repain should happen.
+     *
+     * @param Graphics graphics
+     */
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
@@ -65,6 +125,14 @@ public class Invader extends MovablePanel {
         graphics.drawImage(image, 0, 0, null);
     }
 
+    /**
+     * Get a random column in the space based on:
+     *  - the space width
+     *  - the invader width
+     *  - God Random
+     *
+     * @return int a random positive integer in the bounds of the space
+     */
     private int getRandomColumn() {
         Random rand = new Random();
         return rand.nextInt(Shared.SPACE_WIDTH - (int) dim.getWidth());
