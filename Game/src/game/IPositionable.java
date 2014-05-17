e42b005c4ae996fec1c59ee369489d60da1d8584
Haralan Dobrev <hkdobrev@gmail.com>
2014-05-17 19:11:12 +0300

    Add more interfaces: Dimensional, Positionable and Pane

---
+++ Game/src/game/IPositionable.java
@@ -0,0 +1,18 @@
+package game;
+
+public interface IPositionable {
+
+    /**
+     * Get X coordinate
+     *
+     * @return int x
+     */
+    public int getX();
+
+    /**
+     * Get Y coordinate
+     *
+     * @return int y
+     */
+    public int getY();
+}
